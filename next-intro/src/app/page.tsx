ff8d0340296efb78491e4d8845a16e6eac2e8a5f
Bi <116228374+bianny1107@users.noreply.github.com>
2023-10-17 15:12:35 -0400

    page update

--- next-intro/src/app/page.tsx
+++ next-intro/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import styles from './page.module.css';
 
 export default function Home() {
   return <main className={styles.main}>Bianny Michelle Peña Rosario 2022-0443</main>
