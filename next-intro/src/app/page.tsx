c62b1fbc39eb9de9a95bbeb082f44cba1305d29b
Bi <116228374+bianny1107@users.noreply.github.com>
2023-10-16 23:25:37 -0400

    Page update

--- next-intro/src/app/page.tsx
+++ next-intro/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return <main className={styles.main}>Bianny Michelle Peña Rosario 2022-0443</main>
