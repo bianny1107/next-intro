f95a6e749aab2b5544d756c0fc112e18b096da3f
Bi <116228374+bianny1107@users.noreply.github.com>
2023-10-17 15:40:08 -0400

    page code modified

--- next-intro/src/app/page.tsx
+++ next-intro/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <main className={styles.main}>Bianny Michelle Peña Rosario 2022-0443</main>
+  return(
+    <main className="flex min-h-screen flex-col items-center justify between p-24">
+      <h1>
+        Bianny Michelle Peña Rosario 2022-0443
+      </h1>
+    </main>
+  )
 }
